41b7e065ac86928b856dbff618a5ed80c5c2467b
Aashutosh <aashutosh2899@gmail.com>
2020-06-24 16:11:55 -0700

    added services and restaurant

--- HMS/urls.py
+++ HMS/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("home/",include("home.urls")),
+    path("",include("home.urls")),
     path("account/",include("accounts.urls")),
 
 ]
