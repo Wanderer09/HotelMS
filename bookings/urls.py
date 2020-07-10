ee928ed95f92e4b49b3736ebe7d7f651bdf001df
anand1115 <66914162+anand1115@users.noreply.github.com>
2020-07-11 00:20:58 +0530

    booking2 added

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,6 +4,7 @@ from accounts import views as a
 from home import views as h
 urlpatterns = [
     path('',b.bookings,name='bookings'),
+    path('booking2/',b.booking2,name="bookings2"),
     path('contact/',h.contact,name="contact_us"),
     path('logout/',a.logout,name="logout"),
     path('login/',a.login,name="login"),
--- bookings/views.py
+++ bookings/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def bookings(request):
     return render(request,'bookings/booking.html',{})
+def booking2(request):
+    return render(request,'bookings/booking2.html')
