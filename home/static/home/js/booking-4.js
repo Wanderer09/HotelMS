e43568cd2aded2149f379fa9557769e2d9e84b64
Aashutosh <aashutosh2899@gmail.com>
2020-07-15 13:35:47 -0700

    external js

---
+++ home/static/home/js/booking-4.js
@@ -0,0 +1,21 @@
+
+        function details(x){
+          if(x==0){
+           document.getElementById('credit-card-info').style.display="block";
+          }
+          else{
+              document.getElementById('credit-card-info').style.display="none";
+          }
+          if(x==1){
+           document.getElementById('debit-card-info').style.display="block";
+          }
+          else{
+              document.getElementById('debit-card-info').style.display="none";
+          }
+          if(x==2){
+           document.getElementById('debit-card-info').style.display="none";
+           document.getElementById('credit-card-info').style.display="none";
+          }
+          return false;
+        }
+    
