48c9eeb6f334e2ba4e182e4943e2b4e5feff2178
Aashutosh <aashutosh2899@gmail.com>
2020-07-23 13:59:13 -0700

    updated reception booking page

--- home/static/home/js/bookroom.js
+++ home/static/home/js/bookroom.js
@@ -4,12 +4,48 @@ function validation(){
     var phone = document.getElementById('phone-number').value;
     var town = document.getElementById('town').value;
     var email = document.getElementById('email-address').value;
+    var rooms = document.getElementById('number-rooms').value;
+    var guests = document.getElementById('number-guests').value;
+    var adults = document.getElementById('number-adults').value;
+    var children = document.getElementById('number-children').value;
 
     var firstcheck = /^[A-Za-z. ]{1,}$/;
     var lastcheck = /^[A-Za-z. ]{1,}$/;
     var phonecheck=/^(\+91[\-\s]?)?[0]?(91)?[6789]\d{9}$/;
     var towncheck =/^[A-za-z ]{1,}$/;
     var emailcheck=/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+    var roomscheck=/^[0-9]{1,}$/;
+    var guestscheck=/^[0-9]{1,}$/;
+    var adultscheck=/^[0-9]{1,}$/;
+    var childrencheck=/^[0-9]{1,}$/;
+
+    if (roomscheck.test(rooms)){
+    document.getElementById('user-rooms').style.visibility = "hidden";
+    }else{
+    document.getElementById('user-rooms').style.visibility = "visible";
+    return false;
+    }  
+
+    if (guestscheck.test(guests)){
+    document.getElementById('user-guests').style.visibility = "hidden" ;
+    }else{
+    document.getElementById('user-guests').style.visibility = "visible";
+    return false;
+    }  
+
+    if (adultscheck.test(adults)){
+    document.getElementById('user-adults').style.visibility = "hidden";
+    }else{
+    document.getElementById('user-adults').style.visibility = "visible";
+    return false;
+    }  
+    
+    if (childrencheck.test(children)){
+        document.getElementById('user-children').style.visibility = "hidden";
+    }else{
+        document.getElementById('user-children').style.visibility = "visible";
+        return false;
+    }
 
     if (firstcheck.test(firstname)){
         document.getElementById('user-first').innerHTML ="";
